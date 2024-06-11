f860c0b8099380e9ec78dcfb236849ab5c421c9a
Francielli Mota <francielli.mota@fatec.sp.gov.br>
2024-06-11 18:26:49 -0300

    News exercises

--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -3,6 +3,9 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from "./Home";
 import Atv01 from "./Atv01/Atv01";
 import Atv02 from "./Atv02/Atv02";
+import Atv03 from "./Atv03/Atv03";
+import Atv04 from "./Atv04/Atv04";
+import Atv05 from "./Atv05/Atv05";
 
 export default function MinhasRotas() {
     return (
@@ -11,6 +14,10 @@ export default function MinhasRotas() {
             <Route path="/" element={<Home />}> </Route>
             <Route path="/Atv01" element={<Atv01 />}> </Route>
             <Route path="/Atv02" element={<Atv02 />}> </Route> 
+            <Route path="/Atv03" element={<Atv03 />}> </Route> 
+            <Route path="/Atv04" element={<Atv04 />}> </Route> 
+            <Route path="/Atv05" element={<Atv05 />}> </Route> 
+
         </Routes>
         </BrowserRouter>
     );
